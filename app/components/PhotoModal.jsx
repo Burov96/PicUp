18414500b54ef8297396254ce0178175ea8b6bda
Teodor Burov <burov96@gmail.com>
2024-05-20 23:10:55 +0300

    Just added opacity and scaling effects upon opening an image. Looks crispy as hell now!

--- app/components/PhotoModal.jsx
+++ app/components/PhotoModal.jsx
@@ -1,20 +1,47 @@
 "use client";
-
 import Image from "next/image";
+import { useState, useEffect } from "react";
 
 export default function PhotoModal({ src, alt, onClose }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    // Open the modal after a delay to allow initial rendering
+    const timer = setTimeout(() => {
+      setIsOpen(true);
+    }, 100);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  const handleClose = () => {
+    setIsOpen(false);
+    setTimeout(() => {
+      onClose();
+    }, 500); // Close the modal after 300ms transition
+  };
+
   const handleInnerDivClick = (e) => {
     e.stopPropagation();
   };
+
   if (!src) return null;
+
   return (
     <div
-      className="fixed inset-0 bg-black bg-opacity-80 flex justify-center items-center z-50"
-      onClick={onClose}
+      className={`fixed inset-0 bg-black bg-opacity-80 flex justify-center items-center z-50 transition-opacity duration-500 ease-in-out transform-gpu ${
+        isOpen ? "opacity-100" : "opacity-0 pointer-events-none"
+      }`}
+      onClick={handleClose}
     >
-      <div className="bg-gray-800 p-4 rounded-lg relative border border-gray-600 prop" onClick={handleInnerDivClick}>
+      <div
+        className={`bg-gray-800 p-4 rounded-lg relative border border-gray-600 prop transition-transform duration-500 ease-in-out transform-gpu ${
+          isOpen ? "scale-100" : "scale-0"
+        }`}
+        onClick={handleInnerDivClick}
+      >
         <button
-          onClick={onClose}
+          onClick={handleClose}
           className="text-gray-300 hover:text-white mb-2"
         >
           ❌
